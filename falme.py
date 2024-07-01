7f450c8c2a344a884079eefaf2e5eb432836ec85
Prithvi Chaudhary <104778560+prithvichaudhary@users.noreply.github.com>
2024-07-01 22:00:08 +0530

    Update falme.py

--- falme.py
+++ falme.py
@@ -29,5 +29,59 @@ def remove_match_char(list1, list2):
 	# return the concatenated list with False flag
 	list3 = list1 + ["*"] + list2
 	return [list3, False]
+# Driver code
+if __name__ == "__main__":
+
+	# take first name
+	p1 = input("Player 1 name : ")
+
+	# converted all letters into lower case
+	p1 = p1.lower()
+
+	# replace any space with empty string
+	p1.replace(" ", "")
+
+	# make a list of letters or characters
+	p1_list = list(p1)
+
+	# take 2nd name
+	p2 = input("Player 2 name : ")
+	p2 = p2.lower()
+	p2.replace(" ", "")
+	p2_list = list(p2)
+
+	# taking a flag as True initially
+	proceed = True
+
+	# keep calling remove_match_char function
+	# until common characters is found or
+	# keep looping until proceed flag is True
+	while proceed:
+
+		# function calling and store return value
+		ret_list = remove_match_char(p1_list, p2_list)
+
+		# take out concatenated list from return list
+		con_list = ret_list[0]
+
+		# take out flag value from return list
+		proceed = ret_list[1]
+
+		# find the index of "*" / border mark
+		star_index = con_list.index("*")
+
+		# list slicing perform
+
+		# all characters before * store in p1_list
+		p1_list = con_list[: star_index]
+
+		# all characters after * store in p2_list
+		p2_list = con_list[star_index + 1:]
+
+	# count total remaining characters
+	count = len(p1_list) + len(p2_list)
+
+	# list of FLAMES acronym
+	result = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
 
 
